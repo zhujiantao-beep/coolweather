a027435d90517a27198afcb047aba580368bf309
ZJT <1278998373@qq.com>
2020-06-29 21:50:37 +0800

    修改程序图标和名称。

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.coolweather.android.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public static class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
